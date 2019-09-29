3ba6ca73ae445167967350d1f32b53e72ff5b6a9
ahmadooof <aa224rm@student.lnu.se>
2019-09-29 15:46:24 +0200

    fixing issue about printing head table many times.

--- Workshop 2/src/view/console.java
+++ Workshop 2/src/view/console.java
@@ -1,7 +1,9 @@
 package view;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,11 +11,18 @@ import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 public class console {
+	BufferedWriter writer;
+	BufferedReader reader;
 
 	Scanner sc = new Scanner(System.in);
 	int input;
 	static int id;
-	
+
+	public console() throws IOException {
+		writer = new BufferedWriter(new FileWriter("members.txt", true));
+		reader= new BufferedReader(new FileReader("C:\\Users\\ahmad\\Desktop\\eclipse-workspace\\Workshop 2\\members.txt"));
+	}
+
 	public void printWelcome() {
 		System.out.println("Hi and weclome to the club");
 		System.out.println("1.register member.");
@@ -22,17 +31,31 @@ public class console {
 	}
 
 	public void checkInput() {
-			this.input = sc.nextInt();
+		this.input = sc.nextInt();
 	}
 
 	public void registerMember() throws IOException {
-		BufferedWriter writer = new BufferedWriter(new FileWriter("members.txt", true));
+		// check the head of table if exist, else print it.
+		try {
+			if(reader.readLine() == null) {
+				printHeadOfTable();
+			}
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		writer.write("\n");
 		System.out.println("Enter your name:");
 		String name = sc.next();
-		writer.write(String.format("%-15s", name));
+		writer.write(String.format("%-16s", name));
 		System.out.println("Enter your personal Number:");
 		String personalNumber = sc.next();
 		writer.write(String.format("%-15s", personalNumber));
+
 		writer.close();
+	}
+
+	private void printHeadOfTable() throws IOException {
+		writer.write("Members\t\t\tPersonal Numbers\t\t\tID");
 	}
 }
